d06538ecbf08b5f8ab4f093113892e9f480a693d
JaeJun <jejun97@gmail.com>
2022-08-26 22:03:41 +0900

    hangman

---
+++ pages/slider_and_model_DOM/script.js
@@ -0,0 +1,24 @@
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+//Toggle nav
+
+toggle.addEventListener('click', () => {
+    document.body.classList.toggle('show-nav');
+})
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal');
+})
+close.addEventListener('click', () => {
+    modal.classList.remove('show-modal');
+})
+window.addEventListener('click', (even) => {
+    if (modal.classList.contains('show-modal')) {
+        if (even.target === modal) {
+            modal.classList.remove('show-modal');
+        }
+    }
+});
